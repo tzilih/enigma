318b34afb6dbfefa40cd270d1bdee4f7097d36ac
Tzili <tzili.hod@gmail.com>
2017-07-31 08:57:42 -0700

    last pieces

--- server.js
+++ server.js
@@ -1,16 +1,69 @@
+var CryptoJS = require("crypto-js");
 var express = require('express');
 var app = express();
 
 app.use(express.static('build'));
 app.set('views', './build');
 
-app.get('/hello', (req, res) => {
-	res.send("hello from express server /hello route");
+app.get('/new', (req, res) => {
+  // generate new passphrase
+  const passphrase = getNewPassphrase();
+
+  // attach passphrase as a hash url and redirect
+  res.redirect(`/#${passphrase}`);
+});
+
+app.get('/decrypt/:passphrase/:message', (req, res) => {
+  const encryptedMessage = decodeURIComponent(req.params.message);
+  const passphrase = req.params.passphrase;
+  const decrypted = decrypt(encryptedMessage, passphrase);
+  if(decrypted[2].expDate && !hasExpired(decrypted[2].expDate)) {
+    res.json({name: decrypted[0].name, message: decrypted[1].message, expDate: decrypted[2].expDate});
+  } else {
+      throw 'expired';
+  }
+});
+
+app.get('/encrypt/:passphrase/:name/:message/:expDate?', (req, res) => {
+  const expDate = req.params.expDate;
+  const passphrase = req.params.passphrase;
+  const name = req.params.name;
+  const message = req.params.message;
+  const dataToEncrypt = [{name: name}, {message: message}, {expDate: expDate}];
+  const encrypted = encrypt(dataToEncrypt, passphrase);
+  res.json({encryptedMessage: encrypted});
 });
 
 app.get('*', (req, res) => {
 	res.sendFile(`${process.cwd()}/build/index.html`);
 })
+
+encrypt = (data, passphrase) => {
+  const encryptedData = CryptoJS.AES.encrypt(JSON.stringify(data), passphrase);
+  return encryptedData.toString();
+}
+
+decrypt = (encryptedData, passphrase) => {
+  const bytes = CryptoJS.AES.decrypt(encryptedData.toString(), passphrase);
+  const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+  return decryptedData;
+}
+
+getNewPassphrase = () => {
+  let text = "";
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (var i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  return text;
+}
+
+hasExpired = (expDate) => {
+  today = new Date();
+  expDate = new Date(expDate);
+  today.setHours(0,0,0,0);
+  expDate.setHours(0,0,0,0);
+  return (expDate.getTime() < today.getTime());
+}
 
 const port = process.env.PORT || 4000;
 
--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './assets/react-toolbox/theme.css';
 import theme from './assets/react-toolbox/theme.js';
@@ -8,34 +7,89 @@ import Button from 'react-toolbox/lib/button/Button';
 import DatePicker from 'react-toolbox/lib/date_picker/DatePicker';
 import Dialog from 'react-toolbox/lib/dialog/Dialog';
 import Input from 'react-toolbox/lib/input/Input';
+import Encrypt from "./Encrypt";
 
 class App extends Component {
+  state = { name: '', message: '', active: false, encryptedMessage: '', expDate: '', errorMessage: ''};
+  passphrase = (window.location.hash).substring(1);
+
+  handleChange = (name, value) => {
+    this.setState({...this.state, [name]: value});
+  };
+
+  toggleDialog = () => {
+    this.setState({active: !this.state.active});
+  }
+
+  handleEncrypt = () => {
+    // TODO - use rest or spread operators to assign these values:
+    const name = this.state.name;
+    const message = this.state.message;
+    const expDate = this.state.expDate;
+    fetch(`/encrypt/${this.passphrase}/${name}/${message}/${expDate}`)
+      .then(response => response.json())
+      .then(jsonData => this.setState( {encryptedMessage: jsonData.encryptedMessage} ))
+      .catch(error => {
+        alert('Something went wrong');
+      });
+    this.toggleDialog();
+  }  
+
+  handleDecrypt = () => {
+    const encryptedMessage = this.state.encryptedMessage;
+    const uriEncoded = encodeURIComponent(encryptedMessage);
+    fetch(`/decrypt/${this.passphrase}/${uriEncoded}`)
+      .then(response => response.json())
+      .then(jsonData => this.setState( {errorMessage: jsonData.error, name: jsonData.name, message: jsonData.message, expDate: new Date(jsonData.expDate)} ))
+      .catch(error => {
+        alert("There was an error in getting this message.")
+      });
+    this.toggleDialog();
+  }
+
+  getFormattedDate() {
+    const selectedDate = new Date(1995, 11, 17);
+    const expDate = new Date(this.state.expDate);
+    const year = expDate.getFullYear();
+    const month = expDate.getMonth() + 1;
+    const day = expDate.getDate();
+    const formattedDate = new Date(year, month, day);
+    return selectedDate;
+  }
+
+  dialogActions = [
+    { label: "Close", onClick: this.toggleDialog },
+    { label: "Decrypt", onClick: this.handleDecrypt }
+  ];
+
   render() {
     return (
       <ThemeProvider theme={theme}>
         <div className="App">
           <div className="Form">
-            <Input type='text' label='*Name' name='name'/>
-            <Input type='text' label='*Message' name='name' maxLength={120} />
+            <Input type="text" required label="Name" name="name" value={this.state.name} onChange={this.handleChange.bind(this, "name")}/>
+            <Input type="text" required multiline label="Message" name="message" value={this.state.message} onChange={this.handleChange.bind(this, "message")} maxLength={120} />
             <DatePicker
-            label='Expiration Date'
-            // onChange={this.handleChange.bind(this, 'date1')}
-            // value={this.state.date1}
+            label="Expiration Date"
+            // TODO - set today's date as minDate
+            onChange={this.handleChange.bind(this, "expDate")}
+            value={this.state.expDate}
             sundayFirstDayOfWeek
             />
-          </div>
-          <Button label="Encrypt" raised/>
-          <Button label='Decrypt' onClick={this.handleToggle} raised/>
+          <div className="error"></div>
+          <Button label="Encrypt" onClick={this.handleEncrypt} raised/>
+          <Button label="Decrypt" onClick={this.toggleDialog} raised/>
           <Dialog
-          // actions={this.actions}
-          // active={this.state.active}
-          // onEscKeyDown={this.handleToggle}
-          // onOverlayClick={this.handleToggle}
-          title='My awesome dialog'
-          >
+            actions={this.dialogActions}
+            active={this.state.active}
+            onEscKeyDown={this.toggleDialog}
+            onOverlayClick={this.toggleDialog}
+            title="De/Encrypt">
+            <Input type="text" required multiline label="Message" name="encryptedMessage" value={this.state.encryptedMessage} onChange={this.handleChange.bind(this, "encryptedMessage")}/>
           </Dialog>
-          <p>Your Passphrase - PLACEHOLDER</p>
-          <a href="#">Generate new Passphrase</a>
+          </div>
+          <p>Your Passphrase - {this.passphrase}</p>
+          <a href="/new">Generate new Passphrase</a>
         </div>
       </ThemeProvider>
     );
